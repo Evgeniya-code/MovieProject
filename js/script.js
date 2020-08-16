096c5f429ec561be44ff6d8cacff322a0dbdc7c6
Evgeniya-code <65096372+Evgeniya-code@users.noreply.github.com>
2020-08-16 16:34:01 +1200

    for and if

--- js/script.js
+++ js/script.js
@@ -8,13 +8,26 @@ genres:[],
 privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('Как вы его оцените?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('Как вы его оцените?','');
+for (let i=0; i<2; i++){
+   const a = prompt('Один из последних просмотренных фильмов?',''),
+         b = prompt('Как вы его оцените?','');
 
+        if(a !=null && b != null && a !='' && b !='' && a.length <50){
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else{
+            console.log('error');
+            i--;
+        }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+if(personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+console.log('Вы классический зритель');
+}else if(personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+}else{
+    console.log('Ошибка');
+}
 console.log(personalMovieDB);
